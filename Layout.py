869d28e6ece1ef25eab7dbc0751335ae073b81cc
MaxBlanksby <Max@blanksby.com>
2026-09-23 10:00:16 -0700

    Refactor Layout class constructor to accept layoutName and numqubits parameters

--- Layout.py
+++ Layout.py
@@ -2,21 +2,7 @@ import Graph
 
 
 class Layout:
-    def __init__(self):
+    def __init__(self, layoutName, numqubits):
         self.G = Graph.Graph()
-
-
-    def add_node(self, node):
-        self.G.add_node(node)
-
-    def add_edge(self, edge):
-        self.G.add_edge(edge)
-
-    def get_nodes(self):
-        return self.G.get_nodes()
-
-    def get_edges(self):
-        return self.G.get_edges()
-
-    def draw(self):
-        self.G.drawdisplay()
+        self.layout_algorithm = layoutName
+        self.numqubits = numqubits
